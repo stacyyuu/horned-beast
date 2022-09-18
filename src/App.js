485bedee2b36ba96ca0f6a7cc1976c8e5324fd50
Stacy Yu <stacyyuu@gmail.com>
2022-09-17 19:17:13 -0700

    class1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
 
-function App() {
+class App extends React.Component {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main 
+        title = {'UniWhal'}
+        imageUrl = {'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'}
+        description = {'A unicorn and a narwhal nuzzling their horns'}
+      />
+      <Main 
+        title = {'Rhino Family'}
+        imageUrl = {'https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80'}
+        description = {'Parent rhino with two babies'}
+      />
+      <Footer />  
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+        <footer>Author: Stacy</footer>
+    )
+  }
+}
+
+export default Footer;
